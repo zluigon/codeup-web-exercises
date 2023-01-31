011a7db3094cf4955f86bd32e72a68f59aef5e37
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-01-31 14:40:48 -0500

    circle exercises completed

--- js/circle.js
+++ js/circle.js
@@ -8,17 +8,18 @@
 		getArea: function () {
 			// TODO: complete this method
 			// hint: area = pi * radius^2
-			
-			return; // TODO: return the proper value
+			 let area = Math.PI  * Math.pow(circle.radius,2);
+			return area; // TODO: return the proper value
 		},
 		
 		logInfo: function (doRounding) {
 			// TODO: complete this method.
-			
 			// If doRounding is true, round the result to the nearest integer.
+			if (doRounding === true){
+				console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
+			} else
 			// Otherwise, output the complete value
-			
-			console.log("Area of a circle with radius: " + this.radius + ", is: ");
+			console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
 		}
 	};
 	
@@ -31,7 +32,22 @@
 	console.log("=======================================================");
 	// TODO: Change the radius of the circle to 5.
 	
-	// log info about the circle
+	var circle = {
+		radius: 5,
+		
+		getArea: function () {
+			let area = Math.PI  * Math.pow(circle.radius,2);
+			return area;
+		},
+		
+		logInfo: function (doRounding) {
+			if (doRounding === true){
+				console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
+			} else
+				console.log("Area of a circle with radius: " + this.radius + ", is: " + circle.getArea());
+		}
+	};
+	
 	console.log("Raw circle information");
 	circle.logInfo(false);
 	console.log("Circle information rounded to the nearest whole number");
