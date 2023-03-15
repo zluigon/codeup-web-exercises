48544a2120f17ca7a3288ced63e0f556d4d9691f
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-14 22:13:09 -0400

    functionality finished, needs more styling

--- js/weather_map.js
+++ js/weather_map.js
@@ -12,6 +12,13 @@ $(document).ready(function () {
 		zoom: 5, // starting zoom
 	});
 	
+	// change map style
+	// $("#weather-style").change(function (e){
+	// 	e.preventDefault();
+	// 	let styleValue = $("#weather-style").val();
+	// 	console.log(styleValue);
+	// })
+	
 	let weatherLocale = {
 		lat: 40.98,
 		lon: -74.11,
@@ -33,9 +40,18 @@ $(document).ready(function () {
 		console.log(lngLat);
 		weatherLocale.lon = lng
 		weatherLocale.lat = lat
+		buildWeather();
 	}
 	
 	marker.on('dragend', onDragEnd);
+	
+	map.on('dblclick', (e) => {
+		e.preventDefault()
+		weatherLocale.lon = e.lngLat.lng
+		weatherLocale.lat = e.lngLat.lat
+		buildWeather();
+		marker.setLngLat([weatherLocale.lon, weatherLocale.lat])
+	});
 	
 	$("#myBtn").click(function (e) {
 		e.preventDefault();
@@ -47,6 +63,7 @@ $(document).ready(function () {
 			weatherLocale.lon = results[0]
 			weatherLocale.lat = results[1]
 			buildWeather();
+			marker.setLngLat([weatherLocale.lon, weatherLocale.lat])
 		});
 	});
 	
@@ -72,36 +89,31 @@ $(document).ready(function () {
 				
 				let dayOneSunrise = parseTime(data.current.sunrise);
 				let dayOneSunset = parseTime(data.current.sunset);
-				console.log(dayOneSunrise);
-				console.log(dayOneSunset);
 				
-				$("#current-weather").html(`<h4>Today's Weather: ${dayOneDate}</h4>
-													<h4>${data.current.weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4>
-													<img src="http://openweathermap.org/img/w/${data.current.weather[0].icon}.png">
-													<h5>H: ${Math.floor(data.daily[0].temp.max)}&deg;, L: ${Math.floor(data.daily[0].temp.min)}&deg; </h5>`);
-				$("#current-misc").html(`<h4>Feels like: ${Math.floor(data.current.feels_like)}&deg;</h4>
-													<h4>Humidity: ${Math.floor(data.current.humidity)}%</h4>
-													<h4>Wind Speed: ${Math.floor(data.current.wind_speed)} MPH</h4>
-													<h4>Sunrise/Sunset: ${dayOneSunrise}/ ${dayOneSunset}</h4>`)
-				$("#day-1").html(`<h4>${dayTwoDate}</h4>
-													<h4>${data.daily[1].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4>
-													<img src="http://openweathermap.org/img/w/${data.daily[1].weather[0].icon}.png">
-													<h5>H: ${Math.floor(data.daily[1].temp.max)}&deg;, L: ${Math.floor(data.daily[1].temp.min)}&deg; </h5>`);
-				$("#day-2").html(`<h4>${dayThreeDate}</h4>
-													<h4>${data.daily[2].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4>
-													<img src="http://openweathermap.org/img/w/${data.daily[2].weather[0].icon}.png">
-													<h5>H: ${Math.floor(data.daily[2].temp.max)}&deg;, L: ${Math.floor(data.daily[2].temp.min)}&deg; </h5>`);
-				$("#day-3").html(`<h4>${dayFourDate}</h4>
-													<h4>${data.daily[3].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4>
-													<img src="http://openweathermap.org/img/w/${data.daily[3].weather[0].icon}.png">
-													<h5>H: ${Math.floor(data.daily[3].temp.max)}&deg;, L: ${Math.floor(data.daily[3].temp.min)}&deg; </h5>`);
-				$("#day-4").html(`<h4>${dayFiveDate}</h4>
-													<h4>${data.daily[4].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4>
-													<img src="http://openweathermap.org/img/w/${data.daily[4].weather[0].icon}.png">
-													<h5>H: ${Math.floor(data.daily[4].temp.max)}&deg;, L: ${Math.floor(data.daily[4].temp.min)}&deg; </h5>`);
+				if (data.alerts === undefined) {
+					$(".warning").css("display", "none")
+				} else {
+					$(".warning").css("display", "block");
+					$(".warning_content").html(`<span>${data.alerts[0].description}</span>`);
+				}
 				
+				$("#current-weather").html(`<h4>Today's Weather: ${dayOneDate}</h4><h4>${data.current.weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4><img src="http://openweathermap.org/img/w/${data.current.weather[0].icon}.png"><h5>H: ${Math.floor(data.daily[0].temp.max)}&deg;, L: ${Math.floor(data.daily[0].temp.min)}&deg; </h5>`);
 				
+				$("#current-misc").html(`<h4>Feels like: ${Math.floor(data.current.feels_like)}&deg;</h4><h4>Humidity: ${Math.floor(data.current.humidity)}%</h4><h4>Wind Speed: ${Math.floor(data.current.wind_speed)} MPH</h4><h4>Sunrise: ${dayOneSunrise}</h4><h4>Sunset: ${dayOneSunset}</h4>`)
+				
+				$("#day-1").html(`<h6>${dayTwoDate}</h6><h4>${data.daily[1].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4><img src="http://openweathermap.org/img/w/${data.daily[1].weather[0].icon}.png"><h5>H: ${Math.floor(data.daily[1].temp.max)}&deg;, L: ${Math.floor(data.daily[1].temp.min)}&deg; </h5>`);
+				
+				$("#day-2").html(`<h6>${dayThreeDate}</h6><h4>${data.daily[2].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4><img src="http://openweathermap.org/img/w/${data.daily[2].weather[0].icon}.png"><h5>H: ${Math.floor(data.daily[2].temp.max)}&deg;, L: ${Math.floor(data.daily[2].temp.min)}&deg; </h5>`);
+				
+				$("#day-3").html(`<h6>${dayFourDate}</h6><h4>${data.daily[3].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4><img src="http://openweathermap.org/img/w/${data.daily[3].weather[0].icon}.png"><h5>H: ${Math.floor(data.daily[3].temp.max)}&deg;, L: ${Math.floor(data.daily[3].temp.min)}&deg; </h5>`);
+				
+				$("#day-4").html(`<h6>${dayFiveDate}</h6><h4>${data.daily[4].weather[0].main},  ${Math.floor(data.current.temp)}&deg;</h4><img src="http://openweathermap.org/img/w/${data.daily[4].weather[0].icon}.png"><h5>H: ${Math.floor(data.daily[4].temp.max)}&deg;, L: ${Math.floor(data.daily[4].temp.min)}&deg; </h5>`);
 			});
+		
+		$.get("http://api.openweathermap.org/data/2.5/weather", weatherLocale).done(function (data) {
+			console.log(data);
+			$("#currentLocation").html(`<span class="text-nowrap">Current Location: ${data.name}, ${data.sys.country}</span>`)
+		});
 	}
 	
 	buildWeather([-74.11, 40.98]);
@@ -109,25 +121,4 @@ $(document).ready(function () {
 // })();
 
 
-// $.get("http://api.openweathermap.org/data/2.5/weather", weatherLocale).done(function (data) {
-// 	// const marker = new mapboxgl.Marker({
-// 	// 	draggable: true
-// 	// })
-// 	// 		.setLngLat(results)
-// 	// 		.addTo(map);
-// 	//
-// 	// map.flyTo({
-// 	// 	center: results,
-// 	// 	zoom: 10
-// 	// })
-// 	let html = "";
-// 	html += `<h4>Current location: ${data.name}</h4>`;
-// 	html += `<h4>Current weather: ${data.weather[0].description}</h4>`;
-// 	html += `<h5>Current average wind speed: ${parseInt(data.wind.speed)} knots</h5>`;
-// 	html += `<h5>Current Temperature: ${parseInt(data.main.temp)} &deg;</h5>`;
-// 	html += `<h5>Current humidity: ${parseInt(data.main.humidity)}</h5>`;
-//
-// 	$("#weatherBody").html(html);
-// 	$("#currentLocation").text(`Current location: ${$("#searchInput").val()}`);
-// });
 
