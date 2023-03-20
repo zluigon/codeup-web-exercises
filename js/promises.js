443141a1fb6546bdfa3218b5eaa813f9a95f152d
Luis Gonzalez <luis.nmn.gonzalez@gmail.com>
2023-03-20 18:39:50 -0400

    promises progress

--- js/promises.js
+++ js/promises.js
@@ -1,16 +1,21 @@
-fetch('https://api.github.com/users/zluigon', {headers: {'Authorization': gitHubAPI_token}})
+fetch('https://api.github.com/repos/zluigon/codeup-web-exercises/commits', {headers: {'Authorization': gitHubAPI_token}})
 	.then(response => response.json())
 	.then(data => {
 		console.log(data)
-		console.log(`Last updated: ${data.updated_at}`)
+		console.log(`Last updated: ${data[0].commit.author.date}`)
 	});
 
 let gitUser = (user) => {
-	fetch(`https://api.github.com/users/${user}`,
+	fetch(`https://api.github.com/users/${user}/events`,
 		{headers: {'Authorization': gitHubAPI_token}})
 		.then(response => response.json())
 		.then(data => {
 			console.log(data)
-			console.log(`Last updated: ${data.updated_at}`)
+			console.log(`Last updated: ${data[0].created_at}`)
 		})
 }
+
+
+// https://api.github.com/users/zluigon/events/public
+
+// https://api.github.com/repos/zluigon/codeup-web-exercises/commits
